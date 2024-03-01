660da668c815ce5502b9b6298078c18f21850c6d
rachana <rachana.visavadiya@rocket.chat>
2024-03-01 17:07:28 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzODYyMA/spectral.js?branch_slug=main&token=05733cea-42d8-4f52-85bc-471893194145";
+export default { extends: ruleset };
---
+++ docs/About-2FA.md
@@ -0,0 +1,7 @@
+---
+stoplight-id: c09xm73rtir4f
+---
+
+# About-2FA
+
+The beginning of an awesome article...
